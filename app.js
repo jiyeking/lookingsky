e28252fc01104d12826daa81d228b0e342ec017e
liyongfei <liyongfei@ruaho.com>
2017-08-14 00:23:34 +0800

    工资工具-页面初步完成

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var salaryTool = require('./routes/salaryTool');
 
 var app = express();
 
@@ -22,10 +23,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+//静态资源指定项目名称
+app.use("/lookingsky", express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/salaryTool', salaryTool);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/salaryTool.js
+++ routes/salaryTool.js
@@ -3,4 +3,32 @@
  * Created by JKL on 2017/8/13.
  */
 var express = require("express");
-var router = express.Router()
+var router = express.Router();
+
+
+
+/**
+ * 工具首页
+ */
+router.get("/", function (req, res, next) {
+    res.render("salaryTool/index",{"title" : "个人工资收入最大化计算工具"});
+});
+/**
+ * 计算结果
+ */
+router.get("/result", function (req, res, next) {
+    //计算过程
+    var data = {
+        total : 1111
+    }
+    res.render("salaryTool/result", data);
+});
+
+router.get("/:id", function (req, res) {
+    var data = {};
+    data["id"] = 1;
+    data["total"] = 15000;
+    res.render("salaryTool/salary", data);
+});
+
+module.exports = router;
