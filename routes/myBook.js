595a8aa039ab07371984707e88ae1a26d9780427
jiyeking1 <jiyeking1@gmail.com>
2018-01-01 22:54:31 +0800

    测试mybook-04

--- routes/myBook.js
+++ routes/myBook.js
@@ -8,15 +8,11 @@ var router = express.Router();
 router.all("/addBook", function (req, res) {
 
     //获取参数
-    var data = tools.extend({},req.body);
-    data["reqBody"] = req.body;
-
+    var data = req.body;
     if(!tools.isEmptyObject(req.query)){
         data = req.query;
-        data["type"] = "query";
     }
-    data["liyongfei"] = true;
-    // res.render("salaryTool/salary", data);
+    //doAddBook
     res.json(data);
 });
 module.exports = router;
